48524981c738992fd61a10c457362e13f976f44e
Will <wdportman@gmail.com>
2021-02-04 13:33:50 -0800

    set up problem

---
+++ 844.js
@@ -0,0 +1,17 @@
+/*
+
+844. Backspace String Compare
+
+Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+Input: S = "ab#c", T = "ad#c"
+Output: true
+Explanation: Both S and T become "ac".
+
+*/
+
+const backspaceCompare = function(S, T) {
+
+};
--- runningSum.js
+++ runningSum.js
@@ -1,4 +1,7 @@
 /*
+
+RUNNING SUM:
+
 Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
 
 Return the running sum of nums.
