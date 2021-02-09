a865060872305b644089942da445e41e60847541
Will <wdportman@gmail.com>
2021-02-09 10:01:32 -0800

    Add example of closures

---
+++ tinker.js
@@ -0,0 +1,14 @@
+//Example of closure:
+
+function makeFunc() {
+  var name = 'Mozilla';
+  function displayName() {
+    console.log(name);
+  }
+  return displayName;
+}
+
+var myFunc = makeFunc();
+myFunc();
+
+displayName();
